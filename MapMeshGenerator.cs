6747b4f994e581c7b1b7d542beab3eab643cf480
Daniel Harrington <danielharrington06@outlook.com>
2024-11-01 21:29:05 +0000

    just show clicked point

--- MapMeshGenerator.cs
+++ MapMeshGenerator.cs
@@ -35,21 +35,9 @@ public class MapMeshGenerator : MonoBehaviour
             // convert screen coordinates to world position
             worldClick = Camera.main.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, 0));
             
-            if (!click1Active && !click2Active) {
-                click1 = new Vector3(worldClick.x, worldClick.y, worldClick.z);
-                click1Active = true;
+            Debug.Log("Click: " + worldClick);
             }
-            else if (click1Active && !click2Active) {
-                click2 = new Vector3(worldClick.x, worldClick.y, worldClick.z);
-                click2Active = true;
-            }
-            else if (click1Active && click2Active) {
-                // then save stuff and reset
-                databaseHelper.SaveMapEdge(click1.x, click1.y, click2.x, click2.y);
-                click1Active = false;
-                click2Active = false;
-            }
-        }
+            
         DrawLines();
     }
 
