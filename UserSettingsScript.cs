7c6749f1b4d58820324da2b49ad55af45e17dd64
Daniel Harrington <danielharrington06@outlook.com>
2024-12-09 19:56:24 +0000

    invert scroll direction

--- UserSettingsScript.cs
+++ UserSettingsScript.cs
@@ -7,10 +7,12 @@ public class UserSettingsScript : MonoBehaviour
 {
     public bool floor;
     public bool stepFree;
+    public bool invertScroll;
 
     void Start()
     {
         floor = false; // false is floor 0, true is floor 1
         stepFree = true;
+        bool invertScroll = false;
     }
 }
