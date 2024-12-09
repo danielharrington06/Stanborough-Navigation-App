4711d9675dd4d18e31fe7afc726f5ac90ba2fe22
Daniel Harrington <danielharrington06@outlook.com>
2024-12-09 22:44:18 +0000

    invert scroll direction fixed

--- CameraMovementScript.cs
+++ CameraMovementScript.cs
@@ -7,10 +7,14 @@ public class CameraMovementScript : MonoBehaviour
     private Vector3 dragOrigin; // to hold position of mouse in world space when click
     [SerializeField] private DatabaseHelperScript databaseHelper;
     [SerializeField] private UserSettingsScript userSettings;
+
     [SerializeField]
     private float cameraZoomStep, maxCameraSize, minCameraSize;
+
     private float mapBuffer, mapMaxX, mapMinX, mapMaxY, mapMinY;
+
     void Start() {
+
         // get info for max bounds
         float[] mapBounds = databaseHelper.GetMapBounds();
         mapBuffer = databaseHelper.GetMapBuffer();
@@ -18,6 +22,7 @@ public class CameraMovementScript : MonoBehaviour
         mapMinX = mapBounds[1] - mapBuffer;
         mapMaxY = mapBounds[2] + mapBuffer;
         mapMinY = mapBounds[3] - mapBuffer;
+
         // calculate max camera size
         // the greatest possible world width/height divided by the camera width/height scaled to size 1
         // camera aspect is the half the actual width scaled to size one
@@ -25,29 +30,37 @@ public class CameraMovementScript : MonoBehaviour
         float maxCameraSizeY = (mapMaxY-mapMinY)/(1*2);
         // choose the minimum of the two possible maximums
         maxCameraSize = Math.Min(maxCameraSizeX, maxCameraSizeY);
+
         // get min camera size 
         minCameraSize = Convert.ToSingle(databaseHelper.GetMinCameraSize());
+
         // calculate the zoom step as the range of zoom divided by the number of increments
         int numZoomIncrements = Convert.ToInt32(databaseHelper.GetNumCameraZoomIncrements());
         cameraZoomStep = (maxCameraSize - minCameraSize) / numZoomIncrements;
-
+        
         ResetCamera();
-
+        
     }
-    void Update() {
 
+
+    void Update() {
+        
         PanCamera();
         ZoomScroll();
     }
+
     private void ResetCamera() {
+
         // now start camera at a specific size and coordinates
         Vector3 dbStartCoordinates = databaseHelper.GetCameraStartCoordinates();
         camera.transform.position = new Vector3(dbStartCoordinates.x, dbStartCoordinates.y, camera.transform.position.z);
+
         // make sure size is on one of the increments
         int i = 0;
         while (minCameraSize + (cameraZoomStep * i) < databaseHelper.GetCameraStartSize()) {
             i++;
         }
+
         // now figure out if above or below desired is best
         if (minCameraSize + (cameraZoomStep * i) - databaseHelper.GetCameraStartSize() < databaseHelper.GetCameraStartSize() - minCameraSize + (cameraZoomStep * (i-1))) {
             // i is closer to desired, so set size to i's
@@ -56,76 +69,81 @@ public class CameraMovementScript : MonoBehaviour
         else {
             camera.orthographicSize = minCameraSize + (cameraZoomStep * (i-1));
         }
+
         // clamp camera just in case
         camera.transform.position = ClampCamera(camera.transform.position);
     }
+
     private void PanCamera() {
+
         // save position of the camera in world space when the drag starts (click down)
         if (Input.GetMouseButtonDown(0)) { // drag starts (mouse button DOWN)
             dragOrigin = camera.ScreenToWorldPoint(Input.mousePosition);
         }
+
         // if stil held
         if (Input.GetMouseButton(0)) {
             // calculate difference from drag origin
             Vector3 difference = dragOrigin - camera.ScreenToWorldPoint(Input.mousePosition);
+
             // move camera and clamp
             camera.transform.position = ClampCamera(camera.transform.position + difference);
         }
     }
+
     private void ZoomScroll() {
         // on my mouse each 0.1 of scroll input should be one zoom increment
         double scrollInput = Math.Round(Input.GetAxis("Mouse ScrollWheel"), 1);
-        int invertScroll;
-        if (!userSettings.invertScroll) {
-            // dont invert, so 1
-            invertScroll = 1;
-        }
-        else {
-            // inverted scroll, so -1
-            invertScroll = -1;
-        }
-        // loop until i >= invertScroll (-1 if inverted, 1 if normal) * scroll input (which is usually 0.1 or 0.2)
         if (scrollInput > 0) {
             // zoom in scroll input * 10 num times
-            for (int i = 0; i < invertScroll*scrollInput*10; i++) {
-                ZoomIn();
+            for (int i = 0; i < scrollInput*10; i++) {
+                if (!userSettings.invertScroll) ZoomIn() ; else ZoomOut();
             }
         }
         else if (scrollInput < 0) {
             // zoom in scroll input * 10 num times
             // - as scroll input is negative
-            for (int i = 0; i < -invertScroll*scrollInput*10; i++) {
-                ZoomOut();
+            for (int i = 0; i < -scrollInput*10; i++) {
+                if (!userSettings.invertScroll) ZoomOut() ; else ZoomIn();
             }
         }
     }
+
     // zoom in function - public so can be called with a button click
     public void ZoomIn() {
         // change size
         camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - cameraZoomStep, minCameraSize, maxCameraSize);
+
         // then clamp camera
         camera.transform.position = ClampCamera(camera.transform.position);
     }
+
     // zoom out function - public so can be called with a button click
     public void ZoomOut() {
         // change size
         camera.orthographicSize = Mathf.Clamp(camera.orthographicSize + cameraZoomStep, minCameraSize, maxCameraSize);
+
         // then clamp camera
         camera.transform.position = ClampCamera(camera.transform.position);
     }
+
     private Vector3 ClampCamera(Vector3 targetPosition) {
+
         // get camera width and height (which are both actually half the width and height)
         float cameraHeight = camera.orthographicSize;
         float cameraWidth = camera.orthographicSize * camera.aspect;
+
         // can be done as such as the camera height and width is half the actual
         // define max and min x and y for camera position
         float maxX = mapMaxX - cameraWidth;
         float minX = mapMinX + cameraWidth;
         float maxY = mapMaxY - cameraHeight;
         float minY = mapMinY + cameraHeight;
+
         // then clamp
         float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
         float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
         return new Vector3(newX, newY, targetPosition.z);
     }
 }
