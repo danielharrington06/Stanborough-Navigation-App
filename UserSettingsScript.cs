90bf8de1b2cb25d2ff1125fa8e81aa42d79355fa
Daniel Harrington <danielharrington06@outlook.com>
2025-01-15 17:56:16 +0000

    floor toggle code

--- UserSettingsScript.cs
+++ UserSettingsScript.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserSettingsScript : MonoBehaviour
 {
     public bool floor;
     public bool stepFree;
     public bool invertScroll;
+    [SerializeField] private Text floorText;
 
     void Start()
     {
         floor = false; // false is floor 0, true is floor 1
         stepFree = true;
         invertScroll = false;
+        floorText.text = "0";
+    }
+
+    public void SetFloor0() {
+
+        floor = false;
+        floorText.text = "0";
+    }
+
+    public void SetFloor1() {
+        
+        floor = true;
+        floorText.text = "1";
     }
 }
